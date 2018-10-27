263b0c5e4937fac949386cf9cb716cb55886f4a3
alexanderjones19 <alexanderjones4215@gmail.com>
2018-10-27 01:41:51 -0400

    updated styling

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
     $('form').on('submit', function(event) {
-        event.preventDefault();
-
-        let userScores = [];
-        userScores = $('.question-select').map(function() {
-            return $(this).val().charAt(0);
-        }).get();
-
-        let newUser = {
-            name: $('#name').val().trim(),
-            photo: $('#photo').val().trim(),
-            scores: userScores
+        if ($('#name').val().trim() == '') {
+            alert('Please enter a name.');
+            return false;
         }
+        else {
+            event.preventDefault();
 
-        $.post('/api/friends', newUser, function(data) {
-            $('.modal-body').html('<img src="' + data.photo + '" alt="Your Match" width="400"> <p>' + data.name + '</p>');
-            $('#user-match-modal').modal('show');
-        });
+            let userScores = [];
+            userScores = $('.question-select').map(function() {
+                return $(this).val().charAt(0);
+            }).get();
+
+            let newUser = {
+                name: $('#name').val().trim(),
+                photo: $('#photo').val().trim(),
+                scores: userScores
+            }
+
+            $.post('/api/friends', newUser, function(data) {
+                $('.modal-body').html('<img src="' + data.photo + '" alt="Your Match" width="400"> <p>' + data.name + '</p>');
+                $('#user-match-modal').modal('show');
+            });
+        }
     });
 });
     
